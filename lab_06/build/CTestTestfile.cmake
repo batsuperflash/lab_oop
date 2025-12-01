e831606d07c9945d0f133d1ab6bc3cfb6d24c33e
yana_t <stercaiana@yandex.ru>
2025-12-02 00:52:15 +0300

    Remove build files from repository

--- lab_06/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yana/oop_labs/lab_06
-# Build directory: /Users/yana/oop_labs/lab_06/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(dungeon_tests "/Users/yana/oop_labs/lab_06/build/tests")
-set_tests_properties(dungeon_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/yana/oop_labs/lab_06/CMakeLists.txt;30;add_test;/Users/yana/oop_labs/lab_06/CMakeLists.txt;0;")
